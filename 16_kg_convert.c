db317f646c7ac001e69680b7ab891213f9e7eb8b
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-07-19 13:17:00 +0530

    7/19

---
+++ 16_kg_convert.c
@@ -0,0 +1,10 @@
+// wap to convert weight gram into kg.
+#include <stdio.h>
+void main()
+{
+    float kg, gram;
+    printf("enter weight in gram = ");
+    scanf("%f", &gram); // 1.5
+    kg = gram / 1000;
+    printf("weight in kg = %.1f", kg);
+}
